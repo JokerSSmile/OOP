9b2db7ac3ab9be4ab1a5558afce590a89175c36d
Oleg <patrushev1996@gmail.com>
2016-02-25 21:16:25 +0300

    Added tests, some fixes

---
+++ Lab1/Task 1/Lab1/Lab1.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+char ToLowercase(char in) {
+	if (in <= 'Z' && in >= 'A')
+	{
+		return in - ('Z' - 'z');
+	}
+	return in;
+}
+
+void Replace(const char str[], char find[], char replace[], fstream& fout)
+{
+	unsigned k = 0;
+	unsigned long i = 0;
+	unsigned findLength = strlen(find);
+	unsigned strLength = strlen(str);
+
+	if (findLength > strLength)
+	{
+		return;
+	}
+
+	while (i < strLength)
+	{
+		if (ToLowercase(str[i]) == ToLowercase(find[k]))
+		{
+			k++;
+			i++;
+			if (k == findLength)
+			{
+				fout << replace;
+				k = 0;
+			}
+		}
+		else
+		{
+			if (k == 0)
+			{
+				fout << str[i];
+				k = 0;
+				i++;
+			}
+			else
+			{
+				i -= k - 1;
+				fout << str[i - 1];
+				k = 0;
+			}
+		}
+	}
+}
+
+void FillOutputFile(ifstream& fin, fstream& fout, char find[], char replace[])
+{
+	string line;
+	int lineCount = 0;
+
+	while (getline(fin, line))
+	{
+		if (lineCount != 0)
+		{
+			fout << "\n";
+		}
+		Replace(line.c_str(), find, replace, fout);
+		lineCount++;
+	}
+}
+
+void RunApp(char inputFile[], char outputFile[], char find[], char replace[])
+{
+	ifstream fin(inputFile, ios_base::in);
+	fstream fout(outputFile, ios_base::out);
+	if (fin.is_open() == false || fout.is_open() == false)
+	{
+		cout << "Error while opening files" << endl;
+		return;
+	}
+	FillOutputFile(fin, fout, find, replace);
+	fin.close();
+	fout.close();
+	cout << "Done" << endl;
+}
+
+int main(int argc, char * argv[])
+{
+ 	if (argc == 5)
+	{
+		RunApp(argv[1], argv[2], argv[3], argv[4]);
+	}
+	else
+	{
+		cout << "Input must be:\nLab1.exe <input.txt> <output.txt> <find> <replace>" << endl;
+	}
+	return 0;
+}
---
+++ Lab1/Task 1/Release/test.bat
@@ -0,0 +1,36 @@
+REM
+Lab1.exe
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM
+Lab1.exeabc zxc
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM
+Lab1.exe incorrectName.txt output.txt pa papa
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM
+Lab1.exe input_test1.txt output.txt book film
+IF ERRORLEVEL 1 GOTO err
+FC /b output_test1.txt output.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM
+Lab1.exe input_test2.txt output.txt ma mama
+IF ERRORLEVEL 1 GOTO err
+FC /b output_test2.txt output.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM
+Lab1.exe input_test3.txt output.txt nothing nothing1
+IF ERRORLEVEL 1 GOTO err
+FC /b output_test3.txt output.txt
+IF ERRORLEVEL 1 GOTO err
+
+ECHO Tests are finished successfully.
+EXIT /B
+  
+:err
+ECHO Tests are unsuccessful.
+EXIT /B
