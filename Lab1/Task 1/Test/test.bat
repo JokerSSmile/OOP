55beee28ca3454727601a0f583e484af7bf871a2
Oleg <patrushev1996@gmail.com>
2016-03-22 23:47:38 +0300

    Updated

--- Lab1/Task 1/Test/test.bat
+++ Lab1/Task 1/Test/test.bat
@@ -1,35 +1,38 @@
 ECHO off
+SET program="%1"
+if %program% == "" goto err
+
 REM
-Lab1.exe
+%program%
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM
-Lab1.exeabc zxc
+%program% abc zxc
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM
-Lab1.exe incorrectName.txt output.txt pa papa
+%program% incorrectName.txt output.txt pa papa
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM
-Lab1.exe input_test1.txt output.txt book film
+%program% input_test1.txt output.txt book film
 IF ERRORLEVEL 1 GOTO err
-FC /b output_test1.txt output.txt
+FC output_test1.txt output.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM
-Lab1.exe input_test2.txt output.txt ma mama
+%program% input_test2.txt output.txt ma mama
 IF ERRORLEVEL 1 GOTO err
 FC /b output_test2.txt output.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM
-Lab1.exe input_test3.txt output.txt nothing nothing1
+%program% input_test3.txt output.txt nothing nothing1
 IF ERRORLEVEL 1 GOTO err
 FC /b output_test3.txt output.txt
 IF ERRORLEVEL 1 GOTO err
 
-ECHO Tests are successful.
+ECHO Tests are finished successfully.
 EXIT /B
   
 :err
