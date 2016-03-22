684e24597f5af4df4d4029f626b6af6b407c2cfd
Oleg <patrushev1996@gmail.com>
2016-03-22 23:31:46 +0300

    Updated

--- Lab1/Task 2/Lab1_2/Lab1_2.cpp
+++ Lab1/Task 2/Lab1_2/Lab1_2.cpp
@@ -18,72 +18,59 @@ bool IsNumber(const string& str)
 	return true;
 }
 
-float CalculateDiscriminant(const float& A, const float& B, const float& C)
+bool CalculateRoots(const double& A, const double& B, const double& C, double& root1, double& root2)
 {
-	return B * B - 4 * A * C;
-}
-
-void CalculateAndOutputRoots(const float& A, const float& B, const float& discriminant)
-{
-	ofstream fout("output.txt");
-
-	float firstRoot = (-B + sqrt(discriminant)) / (2 * A);
-	cout << std::setprecision(4) << firstRoot << endl;
-	fout << std::setprecision(4) << firstRoot << endl;
-
-	if (discriminant > 0)
+	double discriminant = B * B - 4 * A * C;
+	if (discriminant < 0)
 	{
-		float secondRoot = (-B - sqrt(discriminant)) / (2 * A);
-		cout << std::setprecision(4) << secondRoot << endl;
-		fout << std::setprecision(4) << secondRoot << endl;
-	}
-
-}
-
-bool RunApp(string& strA, string& strB, string& strC)
-{
-	if (IsNumber(strA) && IsNumber(strB) && IsNumber(strC))
-	{
-		float A = stof(strA);
-		float B = stof(strB);
-		float C = stof(strC);
-
-		if (A == 0)
-		{
-			cout << "There is no real root" << endl;
-			return false;
-		}
-
-		float discriminant = CalculateDiscriminant(A, B, C);
-		if (discriminant < 0)
-		{
-			cout << "Discriminant is less than 0" << endl;
-			return false;
-		}
-
-		CalculateAndOutputRoots(A, B, discriminant);
-		
-	}
-	else
-	{
-		cout << "Incorrect input" << endl;
+		cout << "Discriminant is less than 0" << endl;
 		return false;
 	}
-	return true;
+	root1 = (-B + sqrt(discriminant)) / (2 * A);
+	root2 = (-B - sqrt(discriminant)) / (2 * A);
+
+}
+
+void OutputRoots(const double& root1, const double& root2)
+{
+	ofstream fout("output.txt");
+	cout << std::setprecision(4) << root1 << endl;
+	fout << std::setprecision(4) << root1 << endl;
+	cout << std::setprecision(4) << root2 << endl;
+	fout << std::setprecision(4) << root2 << endl;
 }
 
 int main(int argc, char* argv[])
 {
 	if (argc == 4)
 	{
-		if (!RunApp(string(argv[1]), string(argv[2]), string(argv[3])))
+		string strA = static_cast<string>(argv[1]);
+		string strB = static_cast<string>(argv[2]);
+		string strC = static_cast<string>(argv[3]);
+		if (IsNumber(strA) && IsNumber(strB) && IsNumber(strC))
 		{
+			double A = stof(strA);
+			double B = stof(strB);
+			double C = stof(strC);
+			double root1;
+			double root2;
+			if (A == 0)
+			{
+				cout << "There is no real root" << endl;
+				return 1;
+			}
+			CalculateRoots(A, B, C, root1, root2);
+			OutputRoots(root1, root2);
+		}
+		else
+		{
+			cout << "Incorrect input" << endl;
 			return 1;
 		}
 	}
 	else
 	{
-			cout << "Need 3 argument\nInput must be: Lab1_2.exe <A> <B> <C>\nor\nLab1_2.exe <input_file>" << endl;
+			cout << "Need 3 argument\nInput must be: Lab1_2.exe <A> <B> <C>\n" << endl;
 			return 1;
 	}
 	return 0;
--- Lab1/Task 2/Tests/Lab1_2.bat
+++ Lab1/Task 2/Tests/Lab1_2.bat
@@ -1,24 +1,27 @@
 ECHO off
+SET program="%1"
+if %program% == "" goto err
+
 REM
-Lab1_2.exe 3 8 2
+%program% 3 8 2
 IF ERRORLEVEL 1 GOTO err
 FC /b output_test1.txt output.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM
-Lab1_2.exe 1 -2 -8.3
+%program% 1 -2 -8.3
 IF ERRORLEVEL 1 GOTO err
 FC /b output_test2.txt output.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM
-Lab1_2.exe 5.1 10 -2.1
+%program% 5.1 10 -2.1
 IF ERRORLEVEL 1 GOTO err
 FC /b output_test3.txt output.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM
-Lab1_2.exe 1 10 1
+%program% 1 10 1
 IF ERRORLEVEL 1 GOTO err
 FC /b output_test4.txt output.txt
 IF ERRORLEVEL 1 GOTO err
