e1a0ecc3e3c44c01c3d0db34554277dcd795936d
Oleg <patrushev1996@gmail.com>
2016-05-29 21:55:09 +0300

    Added lab findMaxEx

---
+++ Lab7/FindMaxEx/FindMaxEx.cpp
@@ -0,0 +1,16 @@
+// FindMaxEx.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+template <typename T>
+void FindMaxEx(const std::vector<T>& array)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
+
